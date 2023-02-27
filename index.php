4daaae195dc6e1aa9ac159994fb85482f51d5e03
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-02-27 21:18:49 +0000

    new changes

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <link rel="apple-touch-icon" href="Favicon/apple-touch-icon.png" />
-    <link rel="icon" href="Favicon/favicon-16x16.png" />
-    <link rel="icon" href="Favicon/favicon-32x32.png" />
+    <link rel="apple-touch-icon" href="apple-touch-icon.png"/> 
+    <link rel="icon" href="favicon-16x16.png"/> 
+    <link rel="icon" href="favicon-32x32.png"/>  
     <title>Page Title</title>
   </head>
 
