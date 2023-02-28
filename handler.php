66aacc9e655f399242cb6ceff2a4b1049699c216
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-02-28 14:44:29 +0000

    new changes

--- handler.php
+++ handler.php
@@ -2,17 +2,13 @@
 <html>
   <head>
     <link rel="stylesheet" href="styles.css">
-    <link rel="shortcut icon" type="image/x-icon" href="/php-form-handler-AlexaChase/favicon.ico">
-    <link rel="apple-touch-icon" href="Favicon/apple-touch-icon.png"/>
-    <link rel="icon" href="Favicon/favicon-16x16.png" />
-    <link rel="icon" href="Favicon/favicon-32x32.png" />
     <title>Hello, World! Form</title>
   </head>
 
   <body>
     
     <?php
-    $userName = "fname";
+    $userName = "";
     if ( isset( $_POST['userName'] ) ){
       $userName = $_POST['userName'];
     }
