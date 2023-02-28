8b7e71a9665fcab497db4111e64a63496bb25f9a
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-02-28 02:12:19 +0000

    new changes

--- handler.php
+++ handler.php
@@ -2,9 +2,9 @@
 <html>
   <head>
     <link rel="stylesheet" href="styles.css">
-    <link rel="apple-touch-icon" href="Favicon/apple-touch-icon.png">
-    <link rel="icon" href="Favicon/favicon-16x16.png">
-    <link rel="icon" href="Favicon/favicon-32x32.png">
+    <link rel="apple-touch-icon" href="Favicon/apple-touch-icon.png"/>
+    <link rel="icon" href="Favicon/favicon-16x16.png" />
+    <link rel="icon" href="Favicon/favicon-32x32.png" />
     <title>Hello, World! Form</title>
   </head>
 
